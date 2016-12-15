1599ff7c5a8fdf1920a2743705ec10e1c4a0a0a9
Nguyen Phan <nqphan212@gmail.com>
2016-12-15 13:45:29 -0500

    Player start information

---
+++ Assets/Scripts/Definitions/PlayerSetupDefinition.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class PlayerSetupDefinition{
+	public string Name;
+	public Transform Location;
+	public Color AccentColor;
+	public List<GameObject> StartingUnits = new List<GameObject>();
+	public bool isAi;
+	public float Credits;
+
+}
--- Assets/Scripts/RtsManager.cs
+++ Assets/Scripts/RtsManager.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class RtsManager : MonoBehaviour {
 
 
 	public static RtsManager Current = null;
+	public List<PlayerSetupDefinition> Players = new List<PlayerSetupDefinition>();
 	// Use this for initialization
 	void Start () {
 		Current = this;
