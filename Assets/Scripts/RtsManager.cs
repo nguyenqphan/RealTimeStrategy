d5a42309ab2091c3afafe544cef8659adc90f49e
Nguyen Phan <nqphan212@gmail.com>
2016-12-15 13:08:20 -0500

    Create RtsManager script

---
+++ Assets/Scripts/RtsManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RtsManager : MonoBehaviour {
+
+
+	public static RtsManager Current = null;
+	// Use this for initialization
+	void Start () {
+		Current = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
